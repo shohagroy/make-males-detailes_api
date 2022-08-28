336610b068cf1a8a683a0ab909fcb7686f847f26
unknown <pkshphag240@gmail.com>
2022-08-28 21:14:06 +0600

    js code update

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
 const mainCardDiv = document.querySelector('.card-container');
 
 const allRecipes = (recipes) =>{
-    console.log(recipes)
     recipes.forEach(recipe => {
         displayRecipes(recipe)
+        console.log(recipe.idCategory)
     });
 }
 
@@ -21,11 +21,11 @@ const displayRecipes = (recipe) =>{
 
     cardDiv.innerHTML = `
         <div class="card">
-            <img src="${strCategory}" class="card-img-top" alt="...">
+            <img src="${recipe.strCategoryThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h3 class="card-title">Card title</h3>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title">Category: ${recipe.strCategory}</h5>
+            <h3 class="card-title">ID: ${recipe.idCategory}</h3>
+            <p class="card-text">${recipe.strCategoryDescription}</p>
         </div>
     `
     
